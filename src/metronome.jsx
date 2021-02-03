062f83e809df2dc618734338d3b6c22f162878f8
Rebecca Bakels <rebeccabakels@gmail.com>
2021-02-03 17:11:14 -0500

    started working on metronome project

---
+++ src/metronome.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import './metronome.css'
+
+class Metronome extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          playing: false,
+          bpm: 100  
+        }
+    }
+
+    handleBpmChange = (event) => {
+        const bpm = event.target.value;
+        this.setState({bpm})
+    }
+
+render() {
+
+    const {playing, bpm} = this.state
+
+    return (
+    <div className="metronome">
+        <div className="bpm-slider">
+        <div>{bpm} BPM</div>
+        <input type ="range" min="35" max="250" value={bpm} onChange={this.handleBpmChange}/>
+        </div>
+        <button>{playing? 'stop' : 'start'}</button>
+        </div>
+        )
+    }
+
+}
+
+export default Metronome
