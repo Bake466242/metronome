e3a67eaf85fa4102198963083a54950f64e787ae
Rebecca Bakels <rebeccabakels@gmail.com>
2021-02-04 10:28:58 -0500

    got metronome working

--- src/metronome.jsx
+++ src/metronome.jsx
@@ -1,19 +1,71 @@
 import React, {Component} from 'react'
 import './metronome.css'
+import click1 from './click1.wav'
+import click2 from './click2.wav'
 
 class Metronome extends Component {
     constructor(props) {
         super(props);
 
+        this.click1 = new Audio(click1)
+        this.click2 = new Audio(click2)
+
         this.state = {
+          count: 0,
           playing: false,
-          bpm: 100  
+          bpm: 100,
+          beatsPerMeasure: 4 
         }
     }
 
     handleBpmChange = (event) => {
         const bpm = event.target.value;
+        if (this.state.playing){
+            clearInterval(this.timer)
+            this.timer = setInterval(this.playClick, (60 / bpm) * 1000)
+            this.setState({
+                count: 0,
+                bpm
+            })
+        } else {
         this.setState({bpm})
+        }
+    }
+
+    playClick = () => {
+        const { count, beatsPerMeasure } = this.state
+        if (count % beatsPerMeasure === 0){
+            this.click1.play()
+        } else {
+            this.click2.play()
+        }
+        this.setState(state => ({
+            count: (state.count + 1) % state.beatsPerMeasure
+        }))
+    }
+
+    startStop = () => {
+        //this.click1.play()
+        if (this.state.playing) {
+            //stop logic
+            clearInterval(this.timer)
+            this.setState({
+                playing: false
+            })
+        } else {
+            //start logic
+            this.timer = setInterval(
+                this.playClick,
+                (60 / this.state.bpm) * 1000
+            )
+            this.setState({
+                count: 0,
+                playing: true
+            },
+                this.playClick
+            )
+            
+        }
     }
 
 render() {
@@ -26,7 +78,7 @@ render() {
         <div>{bpm} BPM</div>
         <input type ="range" min="35" max="250" value={bpm} onChange={this.handleBpmChange}/>
         </div>
-        <button>{playing? 'stop' : 'start'}</button>
+        <button onClick={this.startStop}>{playing ? 'stop' : 'start'}</button>
         </div>
         )
     }
